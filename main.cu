021c3a34182d9b5660bba921165b58b91d851d2f
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2026-08-08 23:52:01 +0900

    01: 32个数使用多线程各加10

--- main.cu
+++ main.cu
@@ -6,34 +6,45 @@
 #include <iostream>
 #include <vector>
 
-__global__ void add10(int* data)
+__global__ void add10(int* data, int size)
 {
-    int i = threadIdx.x;
-    data[i] = data[i] + 10;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < size)
+    {
+        data[i] = data[i] + 10;
+    }
 }
 
 int main() {
-    int host_data[8] = {
-        0,1,2,3,4,5,6,7
-    };
+    constexpr int size = 32;
+    int host_data[size];
+    for (int i = 0; i < size; ++i)
+    {
+        host_data[i] = i;
+    }
 
     int* device_data;
 
     cudaMalloc(&device_data, sizeof(host_data));
 
-    cudaMemcpy(device_data, host_data, sizeof(host_data),cudaMemcpyHostToDevice);
+    cudaMemcpy(device_data, host_data, sizeof(host_data), cudaMemcpyHostToDevice);
 
-    add10<<<1,8>>>(device_data);
+    constexpr int block_size = 8;
+
+    int block_count = (size + block_size - 1) / block_size;
+
+    add10<<<block_count, block_size>>>(device_data, size);
 
     cudaDeviceSynchronize();
 
-    cudaMemcpy(host_data, device_data, sizeof(host_data),cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_data, device_data, sizeof(host_data), cudaMemcpyDeviceToHost);
 
     cudaFree(device_data);
 
     for (int value : host_data)
     {
-        std::cout << value << ' ';
+        std::cout << value << " ";
     }
 
     std::cout << '\n';
